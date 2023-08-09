95007f1901bc5c7f560f949fa8b4556720d928d1
Charafeddine Mechalikh <46229052+CharafeddineMechalikh@users.noreply.github.com>
2023-08-09 04:02:15 +0100

    Update export_git_history_multiple.sh

--- export_git_history_multiple.sh
+++ export_git_history_multiple.sh
@@ -12,14 +12,13 @@ output_directory="$current_directory"
 # Output directory for the summary CSV file
 summary_output="$current_directory"
 
+# Remove previous CSV files
+rm -f "$output_directory"/*_git_history.csv
+rm -f "$summary_output"/summary_git_history.csv
+
 # Count the number of repositories
-repo_count=$(find "$repo_directory" -maxdepth 1 -type d | wc -l)
-
-# Initialize progress variables
-current_repo=0
-
-# Declare an associative array to store commit counts for each repository
-declare -A commit_counts
+total_repos=$(find "$repo_directory" -maxdepth 1 -type d | wc -l)
+processed_repos=0
 
 # Loop through each repository in the directory
 for repo in "$repo_directory"/*; do
@@ -32,43 +31,100 @@ for repo in "$repo_directory"/*; do
         # Get the Git log in a format suitable for parsing
         git_log=$(git log --format="%ad" --date=short | sort | uniq -c)
         
+        # Prepare the CSV header
+        echo "Date,Commit Count" > "$output_directory/${repo_name}_git_history.csv"
+        
         # Initialize variables for cumulative commit count
         cumulative_count=0
         
+        # Get total number of lines in the log
+        total_lines=$(echo "$git_log" | wc -l)
+        current_line=0
+        
         # Loop through the Git log entries and extract date and cumulative commit count
         while read -r line; do
+            current_line=$((current_line + 1))
+            
             commit_count=$(echo "$line" | awk '{print $1}')
             date=$(echo "$line" | awk '{print $2}')
             cumulative_count=$((cumulative_count + commit_count))
-            commit_counts["$repo_name,$date"]=$cumulative_count
+            
+            echo "$date,$cumulative_count" >> "$output_directory/${repo_name}_git_history.csv"
+            
+            # Calculate progress percentage
+            progress_percentage=$((current_line * 100 / total_lines))
+            echo -ne "Processing $repo_name: $progress_percentage% ($current_line/$total_lines)\r"
+            
         done <<< "$git_log"
         
-        echo "Git history collected for $repo_name"
+        echo -e "\nGit history exported to ${repo_name}_git_history.csv"
+        processed_repos=$((processed_repos + 1))
         
-        # Update progress
-        current_repo=$((current_repo + 1))
-        progress=$((current_repo * 100 / repo_count))
-        echo -ne "Progress: $progress% \r"
     fi
 done
 
-# Prepare the CSV header for the summary CSV
-header="Date"
-for repo_name in "${!commit_counts[@]}"; do
-    header="$header,$repo_name"
+# Combine individual CSV files into a summary CSV
+summary_csv_header="Date"
+repo_count=0
+for repo_csv in "$output_directory"/*_git_history.csv; do
+    repo_name=$(basename "$repo_csv" _git_history.csv)
+    summary_csv_header+=",$repo_name"
+    repo_count=$((repo_count + 1))
+done
+echo "$summary_csv_header" > "$summary_output/summary_git_history.csv"
+
+# Calculate the total number of dates across all repositories
+total_dates=0
+for repo_csv in "$output_directory"/*_git_history.csv; do
+    repo_dates=$(awk -F',' 'NR>1 {print $1}' "$repo_csv")
+    total_dates=$((total_dates + $(echo "$repo_dates" | wc -w)))
 done
 
-echo "$header" > "$summary_output/summary_git_history.csv"
+# Initialize an empty array to hold all dates
+all_dates=()
 
-# Loop through unique dates and assemble the summary CSV row
-unique_dates=($(cut -d',' -f2 <<< "${!commit_counts[@]}" | sort -u))
-for date in "${unique_dates[@]}"; do
-    row="$date"
-    for repo_name in "${!commit_counts[@]}"; do
-        commit_count=${commit_counts[$repo_name]}
-        row="$row,$commit_count"
+# Loop through the CSV files of each repository
+for repo_csv in "$output_directory"/*_git_history.csv; do
+    repo_dates=$(awk -F',' 'NR>1 {print $1}' "$repo_csv")
+    
+    # Append the dates from the current repository to the all_dates array
+    all_dates=("${all_dates[@]}" $repo_dates)
+    
+    # Print the size of all_dates array after each addition
+    echo "Size of all_dates array: ${#all_dates[@]}"
+done
+
+# Remove duplicates and sort the dates
+unique_sorted_dates=$(echo "${all_dates[@]}" | tr ' ' '\n' | sort -u)
+
+# Loop through the unique and sorted dates
+for date in $unique_sorted_dates; do
+    # Initialize the summary CSV line with the date
+    summary_csv_line="$date"
+    
+     # Loop through the CSV files of each repository
+    for repo_csv in "$output_directory"/*_git_history.csv; do
+        # Extract commit count for the current date and repository
+        commit_count=$(awk -F',' -v date="$date" '{if ($1 == date) print $2}' "$repo_csv")
+        
+        # If commit count is empty, use the last commit count
+        [ -z "$commit_count" ] && commit_count=$last_commit_count
+        
+        # Update last commit count
+        last_commit_count=$commit_count
+        
+        # Append the commit count to the summary CSV line
+        summary_csv_line+=",$commit_count"
     done
-    echo "$row" >> "$summary_output/summary_git_history.csv"
+    
+    # Append the summary CSV line to the summary output file
+    echo "$summary_csv_line" >> "$summary_output/summary_git_history.csv"
+    
+    # Display progress
+    processed_dates=$((processed_dates + 1))
+    progress_percentage=$((processed_dates * 100 / (total_dates * repo_count)))
+    echo -ne "Processing repositories: $progress_percentage% ($processed_dates/$((total_dates * repo_count)))\r"
 done
 
 echo "Summary CSV file created at $summary_output/summary_git_history.csv"
+echo "Processed $processed_dates dates across $repo_count repositories"
